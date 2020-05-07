8716cc412ef005b80553e61daf0944f7ef28ff7e
Esp Illam <mail2dhanasekar13@gmail.com>
2020-05-07 11:20:26 -0700

    initial setup

---
+++ SafeHealth/SafeHealth/HomePage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SafeHealth
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+      async void MainPage(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}
---
+++ SafeHealth/SafeHealth/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SafeHealth
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+       async void DuringPregnancy(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new healthDuring());
+        }
+        async void DangerDuringPregnancy(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new DangerDuringPregnancy());
+        }
+        async void DuringBirth(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new DangerDuringBirth());
+        }
+        async void AfterBirth(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new DangerAfterBirth());
+        }
+    }
+}
